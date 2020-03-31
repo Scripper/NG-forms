7ab5b2ff048f49078d13eb8855f95ae0e42b2bf7
Scripper <nikita.zabaronok@gmail.com>
2020-03-31 18:35:45 +0300

    decomposition

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,52 +1,28 @@
-import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
-import {FormArray, FormBuilder, FormGroup} from '@angular/forms';
-import { Subscription} from 'rxjs';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss'],
 })
-export class FormComponent implements OnInit, OnDestroy {
+export class FormComponent implements OnInit {
   myForm: FormGroup;
-  isDeleteButtonDisabled: boolean;
-  formSubscribe: Subscription;
   @Output () formValues = new EventEmitter();
-  @Input () trigger;
-  constructor(private fb: FormBuilder) {
-    this.isDeleteButtonDisabled = true;
+  constructor() {
+    this.myForm = new FormGroup({
+      firstName: new FormControl(),
+      lastName: new FormControl(),
+      age: new FormControl(),
+      number: new FormControl()
+    });
   }
   ngOnInit(): void {
-    this.myForm = this.fb.group({
-      users: this.fb.array([])
+    this.formValues.emit(this.myForm);
+    this.myForm.valueChanges.subscribe(() => {
+      this.formValues.emit(this.myForm);
     });
-    this.formSubscribe = this.myForm.valueChanges.subscribe((values) => {
-      this.formValues.emit(values.users);
-      if (values.users.length <= 1) {
-        this.isDeleteButtonDisabled = true;
-        return;
-      }
-      this.isDeleteButtonDisabled = false;
-    });
-    this.addFormGroup();
-  }
-  ngOnDestroy(): void {
-    this.formSubscribe.unsubscribe();
-  }
-  get formGroup() {
-    return this.myForm.get('users') as FormArray;
-  }
-  addFormGroup() {
-    const user = this.fb.group({
-      firstName: '',
-      lastName: '',
-      age: null,
-      number: null,
-    });
-    this.formGroup.push(user);
-  }
-  deleteFormGroup(index) {
-    this.formGroup.removeAt(index);
   }
 }
 
--- src/app/forms-group/forms-group.component.ts
+++ src/app/forms-group/forms-group.component.ts
@@ -1,4 +1,6 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, OnDestroy, OnInit, Output} from '@angular/core';
+import {FormArray, FormBuilder, FormGroup} from '@angular/forms';
+import {Subscription} from 'rxjs';
 
 
 @Component({
@@ -6,14 +8,42 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
   templateUrl: './forms-group.component.html',
   styleUrls: ['./forms-group.component.css']
 })
-export class FormsGroupComponent {
+export class FormsGroupComponent implements  OnInit, OnDestroy {
   @Output() sendEventToAddForm = new EventEmitter();
-  formValues: Array<object> = [];
-  constructor() { }
-  getFormValues($event) {
-    this.formValues = $event;
+  myForm: FormGroup;
+  formSubscribe: Subscription;
+  formValues;
+  constructor(private fb: FormBuilder) { }
+  ngOnDestroy(): void {
+    this.formSubscribe.unsubscribe();
   }
+
+  ngOnInit(): void {
+    this.myForm = this.fb.group({
+      users: this.fb.array([])
+    });
+    this.formSubscribe = this.myForm.valueChanges.subscribe(() => {
+
+    });
+  }
+  formGroupController($event, idx) {
+    this.formValues = $event;
+    // this.formGroup.setControl(idx, $event);
+  }
+  get formGroup() {
+    return this.myForm.get('users') as FormArray;
+  }
+
   addFormGroup() {
-    this.sendEventToAddForm.emit('privet');
+    const user = this.fb.group([]);
+    this.formGroup.push(user);
+  }
+
+  deleteFormGroup(index) {
+    this.formGroup.removeAt(index);
   }
 }
+// функция для возврата инишиал состояния с айди встроить в кнопку addformgroup, потом по айди искать и исправлять
+// при клике - получение данных, потом ????
+
+// в myForm нету данных с форм, т.к 37 строка.
